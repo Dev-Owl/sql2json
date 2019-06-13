815e942aa3ce023e2279539f551653a743837cbb
Christian Muehle <info@devowl.de>
2019-06-13 07:42:00 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace sql2json
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                printUsage();
+                return;
+            }
+            try
+            {
+                new Sql2Json(args).Validate().Run();
+            }
+            catch (StopException)
+            {
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error, see message before");
+            }
+
+        }
+
+        private static void printUsage()
+        {
+            Console.WriteLine("sql2json [connectionstring] [query/path to query file] [targetfile]");
+            Console.WriteLine("[targetfile] - optional");
+        }
+    }
+}
